9341284b35705eb17e14b1895161f5514f932eb1
GLODEANU cosmin <100295636+GLODEANUCosmin@users.noreply.github.com>
2024-07-07 16:35:31 +0300

    add base project (#1)

---
+++ src/main/java/com/local/market/my_local_market/MyLocalMarketApplication.java
@@ -0,0 +1,13 @@
+package com.local.market.my_local_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLocalMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLocalMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/local/market/my_local_market/MyLocalMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.market.my_local_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLocalMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
